2595f288c942e9a40dfe9053eaa07638c4f76f00
Kyaw Tun <kyawtun@yathit.com>
2014-07-06 06:45:33 +0800

    init

---
+++ product.js
@@ -0,0 +1,64 @@
+/**
+ * @fileOverview Represent sprint.ly product.
+ */
+
+(function(global) {
+  var sprintly = global.sprintly;
+
+
+
+  /**
+   * Represent sprint.ly product.
+   * @param {sprintly.Service} service
+   * @param {Sprintly.Product} product
+   * @constructor
+   */
+  sprintly.Product = function(service, product) {
+    console.assert(product.id);
+    /**
+     * @protected
+     * @type {sprintly.Service}
+     */
+    this.service = service;
+    /**
+     * @protected
+     * @type {Sprintly.Product}
+     */
+    this.product = product;
+    /**
+     * @protected
+     * @type {ydn.db.Storage}
+     */
+    this.db = new ydn.db.Storage('product-' + product.id, sprintly.Product.schema);
+  };
+
+
+  sprintly.Product.prototype.dispose = function() {
+    if (this.db) {
+      this.db.close();
+      this.db = null;
+      this.service = null;
+    }
+  };
+
+
+  /**
+   * @const
+   * @type {Object}
+   */
+  sprintly.Product.schema = {
+    stores: [{
+      name: 'item',
+      keyPath: 'number',
+      indexes: [{
+        name: 'tags',
+        multiEntry: true
+      }, {
+        name: 'status'
+      }, {
+        name: 'type'
+      }]
+    }]
+  };
+
+})(window || self);
---
+++ service.js
@@ -0,0 +1,155 @@
+  /**
+   * @fileOverview HTTP transport service to sprintly backend.
+   */
+
+(function(global) {
+  global.sprintly = global.sprintly || {};
+  var sprintly = global.sprintly;
+
+
+  /**
+   * Create sprint.ly service.
+   * @constructor
+   */
+  sprintly.Service = function() {
+    /**
+     * @type {?string}
+     * @protected
+     */
+    this.username = null;
+    /**
+     * @type {?string}
+     * @protected
+     */
+    this.authHeader = null;
+  };
+
+
+  /**
+   * @define {string} sprint.ly backend base URL.
+   */
+  sprintly.Service.prototype.baseUrl = 'https://sprint.ly/api/';
+
+
+  /**
+   * Send an HTTP request to sprint.ly backend.
+   * @param {{
+   *   path: string,
+   *   method: ?string,
+   *   params: ?Object,
+   *   body: string|Object
+   * }} options
+   * @returns {Promise} resolve on success with `Respond` object and reject with `Error`. `Respond` object has
+   * `headers`, `body`, `status`.
+   */
+  sprintly.Service.prototype.request = function(options) {
+    var me = this;
+    return new Promise(function(resolve, reject) {
+      if (!me.authHeader) {
+        var e = new Error('Not login');
+        e.name = 'LoginError';
+        reject(e);
+      }
+
+      var xhr = new XMLHttpRequest();
+      var method = options.method || 'GET';
+      var query = [];
+      var params = options.params || {};
+      var url = me.baseUrl + options.path;
+      for (var q in params) {
+        query.push(q + '=' + encodeURIComponent(params[q]));
+      }
+      url += '?' + query.join('&');
+      xhr.open(method, url, true);
+      xhr.setRequestHeader('Authorization', me.authHeader);
+      xhr.onload = function(e) {
+        var raw = {
+          status: xhr.status,
+          statusText: xhr.statusText,
+          body: null,
+          raw: xhr.responseText,
+          headers: {}
+        };
+        try {
+          raw.body = JSON.parse(xhr.responseText);
+        } catch (e) {
+          raw.body = null;
+        }
+        var header_lines = xhr.getAllResponseHeaders().split('\n');
+        for (var i = 0; i < header_lines.length; i++) {
+          var idx = header_lines[i].indexOf(':');
+          if (idx > 0) {
+            var name = header_lines[i].substr(0, idx).toLowerCase();
+            var value = header_lines[i].substr(idx + 1).trim();
+            raw.headers[name] = value;
+          }
+        }
+        resolve(raw);
+      };
+      xhr.send(options.body);
+    });
+
+  };
+
+
+  /**
+   * Set authentication header.
+   * @param {?string} auth authentication header.
+   */
+  sprintly.Service.prototype.setAuthentication = function(auth) {
+    this.authHeader = auth;
+  };
+
+
+  /**
+   * Set authentication header.
+   * @return {?string} authentication header.
+   */
+  sprintly.Service.prototype.getAuthentication = function() {
+    return this.authHeader;
+  };
+
+
+  /**
+   * Get list of products.
+   * @returns {Promise}
+   */
+  sprintly.Service.prototype.listProducts = function() {
+    return this.request({
+      path: 'products.json'
+    }).then(function(resp) {
+      return resp.body;
+    });
+  };
+
+
+  /**
+   * Login to server.
+   * Technically login is not required. Here we are getting ready with default product list.
+   * @param {string} user sprint.ly user id.
+   * @param {string} password sprint.ly password or API key.
+   * @returns {Promise} resolve with list of product on login, reject with `Error`.
+   */
+  sprintly.Service.prototype.login = function(user, password) {
+    var me = this;
+    this.authHeader = 'Basic ' + btoa(user + ':' + password);
+    var respond = this.listProducts();
+    respond.then(function(resp) {
+      // console.log(resp.body);
+    }, function() {
+      me.authHeader = null;
+    });
+    return respond;
+  };
+
+
+  /**
+   * Set user profile.
+   * @param {Object} profile
+   */
+  sprintly.Service.prototype.setProfile = function(profile) {
+    this.authHeader = profile.authentication;
+    this.username = profile.username;
+  };
+
+})(window || self);
